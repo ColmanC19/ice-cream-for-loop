a0d4de731a7c49b7e3aed890b2402035a0de2816
Student <example@example.com>
2019-10-29 13:12:37 -0700

    update code with for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 
   var flavors = ["vanilla", "chocolate", "strawberry", "pistachio"]
-  flavors.forEach(function(flavor){
+  for (var i = 0; i < flavors.length; i++){
+ console.log(flavors.length);
     // var myFlavor = $(flavor).val();
-    $("ul").append("<li class='" + flavor + "'>" + flavor + "</li>");
+    // $("ul").append("<li class='" + flavor[] + "'>" + flavor + "</li>");
+      $("ul").append("<li class='" + flavors + "'>" + flavors[i] + "</li>");
+    }
   });
-});
